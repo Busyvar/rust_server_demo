345b5c071a73aa5171f363d02274f51c9d851a6b
BusyVar <paul.lett@free.fr>
2021-02-11 23:05:05 +0100

    Simplification des dependances et mise a jour du code exemple

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-use actix_web::{App, Error, HttpResponse,HttpServer, web};
+use actix_web::{get, Responder, App, HttpResponse,HttpServer};
 use actix_web::http::{StatusCode};
 
-async fn home() -> Result<HttpResponse, Error> {
-    Ok(
-        HttpResponse::build(StatusCode::OK)
-        .content_type("text/html ; charset=utf-8")
-        .body(include_str!("../templates/index.html"))
-      )
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::build(StatusCode::OK)
+    .content_type("text/html ; charset=utf-8")
+    .body(include_str!("../templates/index.html"))
 }
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main()-> std::io::Result<()> {
-    HttpServer::new(move || {
-        App::new()
-            .route("/", web::get().to(home))
-    })
-    .bind("127.0.0.1:8888")?
+    HttpServer::new(|| App::new().service(home))
+        .bind("127.0.0.1:8888")?
         .run()
         .await
 
 }
+
