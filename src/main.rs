b7e9e29b6bc0a7329f3d112c483ee18df4cef905
BusyVar <paul.lett@free.fr>
2021-04-21 23:01:16 +0200

    lint & extend examples

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-use actix_web::{get, Responder, App, HttpResponse,HttpServer};
+use actix_web::{web, Responder, App, HttpResponse,HttpServer};
 use actix_web::http::{StatusCode};
 
-#[get("/")]
 async fn home() -> impl Responder {
     HttpResponse::build(StatusCode::OK)
-    .content_type("text/html ; charset=utf-8")
-    .body(include_str!("../templates/index.html"))
+        .content_type("text/html ; charset=utf-8")
+        .body(include_str!("../templates/index.html"))
+}
+
+async fn greet() -> impl Responder {
+    const MSG: &str = "Hello!";
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/html ; charset=utf-8")
+        .body(MSG)
 }
 
 #[actix_web::main]
 async fn main()-> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(home))
-        .bind("127.0.0.1:8888")?
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/hello", web::get().to(greet))
+            .route("/home", web::get().to(home))
+    })
+    .bind("127.0.0.1:8888")?
         .run()
         .await
 
