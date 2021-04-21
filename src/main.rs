6246e9dfe6c111e899ca0a72390fc098950b6d4c
BusyVar <paul.lett@free.fr>
2021-04-21 23:41:34 +0200

    Add API route with parameters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde::Deserialize;
 use actix_web::{web, Responder, App, HttpResponse,HttpServer};
 use actix_web::http::{StatusCode};
 
@@ -14,6 +15,19 @@ async fn greet() -> impl Responder {
         .body(MSG)
 }
 
+#[derive(Deserialize)]
+pub struct Something {
+    name: String,
+    nick: String
+}
+
+//curl -X GET "http://127.0.0.1:8888/parameterized?name=ma&nick=Baker"
+async fn parameterized(info: web::Query<Something>) -> impl Responder {
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/html ; charset=utf-8")
+        .body( format!("call her {} {}", info.name, info.nick))
+}
+
 #[actix_web::main]
 async fn main()-> std::io::Result<()> {
     HttpServer::new(|| {
@@ -21,6 +35,7 @@ async fn main()-> std::io::Result<()> {
             .route("/", web::get().to(greet))
             .route("/hello", web::get().to(greet))
             .route("/home", web::get().to(home))
+            .route("/parameterized", web::get().to(parameterized))
     })
     .bind("127.0.0.1:8888")?
         .run()
